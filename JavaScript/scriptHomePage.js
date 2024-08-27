e0e96d98d67386c0b4ed16237441dc383829948d
winyu.p@kkumail.com <96216528+Winyu64@users.noreply.github.com>
2024-08-27 12:32:28 +0700

    01

--- JavaScript/scriptHomePage.js
+++ JavaScript/scriptHomePage.js
@@ -13,3 +13,38 @@ function changeBackground() {
 
 setInterval(changeBackground, 3500); 
 changeBackground();
+
+
+
+
+function toggleChatbot() {
+    var chatbotWindow = document.getElementById("chatbotWindow");
+    chatbotWindow.classList.toggle("hidden");
+}
+
+function sendMessage() {
+    var userMessage = document.getElementById("userMessage").value;
+    var chatbotBody = document.getElementById("chatbotBody");
+
+    if (userMessage.trim() !== "") {
+        // แสดงข้อความของผู้ใช้
+        var userMessageElement = document.createElement("div");
+        userMessageElement.className = "user-message";
+        userMessageElement.textContent = userMessage;
+        chatbotBody.appendChild(userMessageElement);
+
+        // แสดงการตอบกลับของแชทบอท
+        var botMessageElement = document.createElement("div");
+        botMessageElement.className = "bot-message";
+        botMessageElement.textContent = "สวัสดี";
+        chatbotBody.appendChild(botMessageElement);
+
+        // เลื่อนการแสดงผลไปที่ข้อความล่าสุด
+        chatbotBody.scrollTop = chatbotBody.scrollHeight;
+
+        // ล้างข้อความใน input
+        document.getElementById("userMessage").value = "";
+    }
+}
+
+
